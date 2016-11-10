d50e6b496e4f14d8eb4a5e15407c946ea3cb1de5
Andre Dotto <andrecdot@gmail.com>
2016-11-10 10:38:54 -0200

    kbml tuning

--- R/AlradSpectra.R
+++ R/AlradSpectra.R
@@ -423,13 +423,14 @@ AlradSpectra <- function() {
   dispose(alert)
   gmessage("KBML model done", title = "KBML model", parent = window)
   }
-  fkbmllinear  <- function(...) {kbml.test    <<- caret::train(form.mdl, data = Train, method = 'gaussprLinear',trControl = bootctrl.kbml,  na.action = na.omit)
+  fkbmllinear  <- function(...) {kbml.test    <<- caret::train(form.mdl, data = Train, method = 'gaussprLinear', trControl = bootctrl.kbml,
+                                                               na.action = na.omit,  preProcess = c("center", "scale"), tuneLength = 10)
   kbml.model    <<- kernlab::gausspr(form.mdl, data=Train, kernel= "vanilladot", type = "regression", kpar= "automatic", scaled = T,
                             variance.model = T, var=as.numeric(svalue(kbml.var)), cross= svalue(kbml.cross))
   }
   fkbmlradial  <- function(...) {Grid          <- expand.grid(.sigma = seq(.00001,.1,.005))
   kbml.test    <<- caret::train(form.mdl, data = Train, method = 'gaussprRadial',  na.action = na.omit,
-                         trControl = bootctrl.kbml, tuneGrid = Grid)
+                         preProcess = c("center", "scale"), tuneLength = 10,  trControl = bootctrl.kbml, tuneGrid = Grid)
   kbml.model   <<- kernlab::gausspr(form.mdl, data=Train, kernel="rbfdot", type ="regression",
                            kpar= "automatic", variance.model = T, scaled = T,
                            var=svalue(kbml.var), cross= svalue(kbml.cross))
